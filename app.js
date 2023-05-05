7cc8b242a0d86472df9541017f19806b75d58da5
Yen Lin <lynn575938@outlook.com>
2023-05-05 15:53:57 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// 僅在非正式環境時，使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send(`hello world!`)
