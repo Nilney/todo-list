b750336fbc7906773ba261fa73beaf4a5d374674
Yen Lin <lynn575938@outlook.com>
2023-07-06 17:54:30 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
@@ -11,7 +12,12 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({ extended: true })) //直接從express呼叫
+app.use(session({
+  secret: 'This is my secret',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(express.urlencoded({ extended: true })) //直接從 express 呼叫 body-parser
 app.use(methodOverride('_method')) // 設定每一筆請求都會先以 methodOverride 進行前置處理
 
 app.use(routes)
